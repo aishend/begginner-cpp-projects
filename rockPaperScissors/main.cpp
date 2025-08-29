31a3514f37cbe0223c4e3a9965bc78d07213e445
Leandro <leandroafonsopm@gmail.com>
2025-08-29 21:22:18 +0100

    rock, paper or scissors?

---
+++ rockPaperScissors/main.cpp
@@ -0,0 +1,71 @@
+#include <iostream>
+#include <string>
+#include <map>
+using namespace std; 
+
+int getComputerPlay() {
+    // gera um numero aleatorio semeado!  
+    srand(time(NULL));
+    return rand() % 3;
+}
+
+bool win(string player, string secondary)
+{
+    if (player == "scissors" && secondary == "paper") {
+        return true;
+    } 
+    if (player == "rock" && secondary == "scissors") {
+        return true;
+    } 
+    if (player == "paper" && secondary == "rock") {
+        return true;
+    } 
+    return false;   
+} 
+
+int main() 
+{
+    
+    int number; 
+    cout << "ROCK! PAPER ORRRR... SCISSORS!!!???" << endl;
+    cout << "What is invencible weapon?" << endl; 
+    while(1) 
+    {
+        cout << "scissors - 1" << endl;
+        cout << "paper - 2" << endl;
+        cout << "rock - 3" << endl;
+
+        cout << "Choose a number! (1 - 3) "; 
+        cin >> number;
+        
+        if (number > 0 && number < 4) {
+            break;
+        }
+        else {
+            cout << "Your input is not valid! Please try again!" << endl;
+            cout << endl; 
+        }
+    }
+
+
+    map<int, string> dict; 
+    dict[0] = "scissors";
+    dict[1] = "paper";
+    dict[2] = "rock";
+     
+    int robotPlay = getComputerPlay();
+    cout << "You: " << dict[number - 1] << " vs Robot: " << dict[robotPlay] << endl;   
+    if (robotPlay == number) {
+        cout << "it was a TIE" << endl;
+    } 
+
+    if (win(dict[number - 1], dict[robotPlay])) {
+        cout << "You won dumbass!!" << endl;
+    }
+    else {
+    cout << "Loser! Better not to try again" << endl;
+    }
+
+    return 0; 
+
+}
